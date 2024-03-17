89d5f02ce6d7d83ac102951c536be33124891d8b
Henjie Agadan <70084057+hagdn@users.noreply.github.com>
2024-03-17 13:45:54 +0800

    Add files via upload
    This section covers data movement:
    1. Register - Register
    2. Memory - Register
    3. Memory - Memory
    4. Register - Memory
    
    The topic on accessing the data memory of MSP43FR2433 is also covered in this section. As well as how bytes, low and high-bytes are read/stored. Another is directly adding data to an address, this includes accessing the data memory starting at address 0x002000 and writing to it.
    
    Overall, for the MSP430FR2433, the program memory starts at memory address 0x00C400, the the syntax for data movement is the opcode then the operands. Operands is in the order of source then destination as well as the data flow. It is also capable of register-register, memory-memory, register<>memory, operations. Although you can only write to the specific registers R0-R3 and you are free to do any on the general purpose registers R4-R15 which are all 16-bit.

---
+++ DataMove_1/main.asm
@@ -0,0 +1,102 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+											; KEY NOTE - Registers are 16 bit
+											; Notice that program memory starts
+											; the memory address 0x00C400
+
+main:		mov.b	R0, R4					; Operands are flipped for MSP430
+											; From Z80 it is from right to left
+											; MSP430 is left to right, i.e.
+											; source -> destination
+
+											; .w indicates 16-bit operation
+											; .b indicates 8-bit operation
+			mov.w	R0, R5
+
+											; IMMEDIATE ADDRESSING with '#'
+											; Values with '#' is the actual
+											; value or data/information.
+
+			mov.b	#'A', R6				; Store the Hex equivalent of ASCII
+			mov.b	#'1', R7
+
+			mov.w	#0FFFFh, R8				; #(value) dictates direct copy/move
+       		mov.w	#0Fh, R9				; of the value to a destination
+
+       		mov.b	#0FFAAh, R10			; low-byte is stored/read first
+
+       										; ABSOLUTE ADDRESSING with '&'
+       										; Values preceding '&' is the
+       										; Actual address
+
+       		mov.b	&2000h, R4				; ASCII is a byte, copy it to R4
+       		mov.w	&2002h, R5				; Although ASCII is a byte,
+       										; it still takes 2 bytes in memory
+       										; this explains instead of ending
+       										; at 2000h, it still ends at 2001h
+       										; hence, FACEh, starts at 2002h
+
+       										; MEMORY-MEMORY data movement
+       		mov.w	&2002h, &2004h			; And next address in data memory
+       										; is at 2004h.
+
+			mov.b	R4, &2006h				; REGISTER-MEMORY data movement
+											; noting that the low-byte of
+											; data stored at memory &2006h
+											; is the only one overwritten.
+											; So high-byte is kept/untouched.
+
+			jmp main
+			NOP
+                                            
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+			.data							; Access data memory
+			.retain							; Notice data memory
+											; starts at 0x002000
+
+var1:		.short	'C'						; Store 'C' into the first address
+var2:		.short	0FACEh					; Store this to the next address
+											; after 'C' or the previous
+
+											; Notice that data are stored
+											; automatically in an orderlly
+											; fashion from the start of
+											; data memory
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
