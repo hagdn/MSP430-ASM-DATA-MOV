28abb52d2e9c4ffa69fe11311b33386e25081aaa
Henjie Agadan <70084057+hagdn@users.noreply.github.com>
2024-03-20 16:45:09 +0800

    Update main.asm

--- DataMove_3/main.asm
+++ DataMove_3/main.asm
@@ -25,40 +25,40 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 main:
 
-			mov.w	#array1, R4				; Takes the address of the firstmost
-											; data of array1 then set it as
-											; value of R4, this address is 16-bit
+			mov.w	#array1, R4	; Takes the address of the firstmost
+						; data of array1 then set it as
+						; value of R4, this address is 16-bit
 
-											; Notice that awareness on how much
-											; bytes a data uses is important as
-											; the n in n(Rx) dictates the amount
-											; of steps the index must move for the
-											; Moving by twos when data are acutally
-											; separate 1-bytes, and only one is
-											; needed to be copied results to
-											; copying/moving of the two data
-											; While moving by 1's when 2-bytes are
-											; needed to be copied results to
-											; only the low-byte being copied
+						; Notice that awareness on how much
+						; bytes a data uses is important as
+						; the n in n(Rx) dictates the amount
+						; of steps the index must move for the
+						; Moving by twos when data are acutally
+						; separate 1-bytes, and only one is
+						; needed to be copied results to
+						; copying/moving of the two data
+						; While moving by 1's when 2-bytes are
+						; needed to be copied results to
+						; only the low-byte being copied
 			mov.w	0(R4), R5
 			mov.w	2(R4), R6
 			mov.w	4(R4), R7
 			mov.w	6(R4), R8
 			mov.w	8(R4), R9
 
-			mov.b	7(R4), R10				; Copies only the high-byte of DECAh
-											; It is a byte-access so it is possible
-											; to do this.
+			mov.b	7(R4), R10	; Copies only the high-byte of DECAh
+						; It is a byte-access so it is possible
+						; to do this.
 
-			mov.w	8(R4), R11				; It is expected here that the high-byte
-											; of DECAh is first copied then the
-											; low-byte of 00AAh, but in MSP430
-											; word-access and byte-access has
-											; different endianess. In byte-access
-											; the low-bytes are read/stored first.
-											; for word access, it is the whole
-											; 2-bytes with the high-byte taken
-											; as a priority.
+			mov.w	8(R4), R11	; It is expected here that the high-byte
+						; of DECAh is first copied then the
+						; low-byte of 00AAh, but in MSP430
+						; word-access and byte-access has
+						; different endianess. In byte-access
+						; the low-bytes are read/stored first.
+						; for word access, it is the whole
+						; 2-bytes with the high-byte taken
+						; as a priority.
 
 
 			jmp		main
@@ -72,7 +72,7 @@ main:
 			.retain
 
 array1:		.short	'A','1','@',0DECAh,0AAAAh
-											; 00 01, 02 03, 04 05, 06 07, 08 09
+						; 00 01, 02 03, 04 05, 06 07, 08 09
 res:		.space	8
 
 ;-------------------------------------------------------------------------------
