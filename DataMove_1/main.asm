2efeefbcf4652d7a82b4c2c6626c79802fcf4e41
Henjie Agadan <70084057+hagdn@users.noreply.github.com>
2024-03-20 16:48:40 +0800

    Update main.asm

--- DataMove_1/main.asm
+++ DataMove_1/main.asm
@@ -23,51 +23,51 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-											; KEY NOTE - Registers are 16 bit
-											; Notice that program memory starts
-											; the memory address 0x00C400
+						; KEY NOTE - Registers are 16 bit
+						; Notice that program memory starts
+						; the memory address 0x00C400
 
-main:		mov.b	R0, R4								; Operands are flipped for MSP430
-											; From Z80 it is from right to left
-											; MSP430 is left to right, i.e.
-											; source -> destination
+main:		mov.b	R0, R4			; Operands are flipped for MSP430
+						; From Z80 it is from right to left
+						; MSP430 is left to right, i.e.
+						; source -> destination
 
-											; .w indicates 16-bit operation
-											; .b indicates 8-bit operation
+						; .w indicates 16-bit operation
+						; .b indicates 8-bit operation
 			mov.w	R0, R5
 
-											; IMMEDIATE ADDRESSING with '#'
-											; Values with '#' is the actual
-											; value or data/information.
+						; IMMEDIATE ADDRESSING with '#'
+						; Values with '#' is the actual
+						; value or data/information.
 
-			mov.b	#'A', R6						; Store the Hex equivalent of ASCII
+			mov.b	#'A', R6	; Store the Hex equivalent of ASCII
 			mov.b	#'1', R7
 
-			mov.w	#0FFFFh, R8						; #(value) dictates direct copy/move
-       		mov.w	#0Fh, R9							; of the value to a destination
+			mov.w	#0FFFFh, R8	; #(value) dictates direct copy/move
+       		mov.w	#0Fh, R9		; of the value to a destination
 
-       		mov.b	#0FFAAh, R10							; low-byte is stored/read first
+       		mov.b	#0FFAAh, R10		; low-byte is stored/read first
 
-       											; ABSOLUTE ADDRESSING with '&'
-       											; Values preceding '&' is the
-       											; Actual address
+       						; ABSOLUTE ADDRESSING with '&'
+       						; Values preceding '&' is the
+       						; Actual address
 
-       		mov.b	&2000h, R4							; ASCII is a byte, copy it to R4
-       		mov.w	&2002h, R5							; Although ASCII is a byte,
-       											; it still takes 2 bytes in memory
-       											; this explains instead of ending
-       											; at 2000h, it still ends at 2001h
-       											; hence, FACEh, starts at 2002h
+       		mov.b	&2000h, R4		; ASCII is a byte, copy it to R4
+       		mov.w	&2002h, R5		; Although ASCII is a byte,
+       						; it still takes 2 bytes in memory
+       						; this explains instead of ending
+       						; at 2000h, it still ends at 2001h
+       						; hence, FACEh, starts at 2002h
 
-       											; MEMORY-MEMORY data movement
-       		mov.w	&2002h, &2004h							; And next address in data memory
-       											; is at 2004h.
+       						; MEMORY-MEMORY data movement
+       		mov.w	&2002h, &2004h		; And next address in data memory
+       						; is at 2004h.
 
-			mov.b	R4, &2006h						; REGISTER-MEMORY data movement
-											; noting that the low-byte of
-											; data stored at memory &2006h
-											; is the only one overwritten.
-											; So high-byte is kept/untouched.
+			mov.b	R4, &2006h	; REGISTER-MEMORY data movement
+						; noting that the low-byte of
+						; data stored at memory &2006h
+						; is the only one overwritten.
+						; So high-byte is kept/untouched.
 
 			jmp main
 			NOP
@@ -75,18 +75,18 @@ main:		mov.b	R0, R4								; Operands are flipped for MSP430
 ;-------------------------------------------------------------------------------
 ; Data Memory
 ;-------------------------------------------------------------------------------
-			.data								; Access data memory
-			.retain								; Notice data memory
-											; starts at 0x002000
+			.data			; Access data memory
+			.retain			; Notice data memory
+						; starts at 0x002000
 
-var1:		.short	'C'								; Store 'C' into the first address
-var2:		.short	0FACEh								; Store this to the next address
-											; after 'C' or the previous
+var1:		.short	'C'			; Store 'C' into the first address
+var2:		.short	0FACEh			; Store this to the next address
+						; after 'C' or the previous
 
-											; Notice that data are stored
-											; automatically in an orderlly
-											; fashion from the start of
-											; data memory
+						; Notice that data are stored
+						; automatically in an orderlly
+						; fashion from the start of
+						; data memory
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
